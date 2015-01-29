67f65b1a6bf12e734f3423b0a923ae9df9353a58
Samuel Borowsky <samuelborowsky@Samuels-MacBook-Pro.local>
2015-01-28 22:04:18 -0500

    Initial Commit

---
+++ Scoop©/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scoop©
+//
+//  Created by Samuel Borowsky on 1/28/15.
+//  Copyright (c) 2015 Sam Borowsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Scoop©/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scoop©
+//
+//  Created by Samuel Borowsky on 1/28/15.
+//  Copyright (c) 2015 Sam Borowsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
